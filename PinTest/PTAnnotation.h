6352fee634ea5a0f6371b6f4306dbc2529d12471
young1park <lossefanya@gmail.com>
2015-07-06 10:07:57 +0200

    map & annotation

---
+++ PinTest/PTAnnotation.h
@@ -0,0 +1,18 @@
+//
+//  PTAnnotation.h
+//  PinTest
+//
+//  Created by Young One Park on 2015. 7. 6..
+//  Copyright (c) 2015년 young1park. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+
+@interface PTAnnotation : NSObject <MKAnnotation>
+
+@property (nonatomic) CLLocationCoordinate2D coordinate;
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, copy) NSString *subtitle;
+
+@end
--- PinTest/ViewController.m
+++ PinTest/ViewController.m
@@ -6,9 +6,13 @@
 //  Copyright (c) 2015년 young1park. All rights reserved.
 //
 
+#import <MapKit/MapKit.h>
 #import "ViewController.h"
+#import "PTAnnotation.h"
 
-@interface ViewController ()
+@interface ViewController () <MKMapViewDelegate>
+
+@property (nonatomic, weak) MKMapView *mapView;
 
 @end
 
@@ -16,12 +20,51 @@
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+	MKMapView *mapView = [[MKMapView alloc] initWithFrame:self.view.bounds];
+	mapView.delegate = self;
+	mapView.translatesAutoresizingMaskIntoConstraints = YES;
+	mapView.autoresizingMask = (UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth);
+	[self.view addSubview:mapView];
+	self.mapView = mapView;
+	
+	UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap:)];
+	tapRecognizer.numberOfTapsRequired = 1;
+	tapRecognizer.numberOfTouchesRequired = 1;
+	[self.mapView addGestureRecognizer:tapRecognizer];
 }
 
 - (void)didReceiveMemoryWarning {
 	[super didReceiveMemoryWarning];
 	// Dispose of any resources that can be recreated.
 }
+
+#pragma mark - Event handlers
+
+- (void)tap:(UITapGestureRecognizer *)sender {
+	CGPoint point = [sender locationInView:self.mapView];
+	CLLocationCoordinate2D tapPoint = [self.mapView convertPoint:point toCoordinateFromView:self.view];
+	PTAnnotation *annotation = [PTAnnotation new];
+	annotation.coordinate = tapPoint;
+	[self.mapView addAnnotation:annotation];
+}
+
+#pragma mark - MKMapViewDelegate
+
+- (MKAnnotationView *)mapView:(MKMapView *)map viewForAnnotation:(id <MKAnnotation>)annotation {
+	MKPinAnnotationView *annotationView = (MKPinAnnotationView *)[map dequeueReusableAnnotationViewWithIdentifier:@"annotationViewID"];
+	
+	if (annotationView == nil) {
+		annotationView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"annotationViewID"];
+	}
+	
+	annotationView.annotation = annotation;
+	annotationView.canShowCallout = YES;
+	annotationView.animatesDrop = YES;
+	
+	return annotationView;
+}
+
+
 
 @end
